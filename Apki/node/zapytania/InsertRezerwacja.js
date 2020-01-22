3f31d8fb8a2b57d3b8f9a64dcdf4bdbb6f86d35a
ddoox <dudek746@gmail.com>
2020-01-22 14:50:56 +0100

    sala pokaz zajete

---
+++ Apki/node/zapytania/InsertRezerwacja.js
@@ -0,0 +1,26 @@
+// Placeholder
+
+const router = require('express').Router()
+const client = require('./Polaczenie')
+
+
+
+router.post('/:nazwa/:id_sala/:data/:link', (req, res) => {
+    const {nazwa,id_sala,data,link} = req.params
+    const odszyfrowanyLink = link.replace(/TuBylaKropkaNieMaToJakSwietnyKod/g,".").replace(/ToSieNazywaDlugiUrl/g,":").replace(/CzasNaSlashe/g,"/")
+        .replace(/JeszczePodkresleniaDzialaAle/g,"_").replace(/JakbyCosToNiePisalemTegoFragmentu/g,",")
+  
+    try{
+        let queryText = 'INSERT INTO wydarzenie(id_sala, data, nazwa, link_obrazek) VALUES ($1, $2, $3, $4)'
+        let queryParams = [id_sala, data, nazwa, odszyfrowanyLink]
+        client.query(queryText, queryParams)
+    }
+    catch(err){
+        console.error(err)
+        res.status(500).json({err})
+    }
+})
+
+
+
+module.exports = router
--- Apki/stojgora/src/komponent/Sala.js
+++ Apki/stojgora/src/komponent/Sala.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react'
 import Button from 'react-bootstrap/Button';
 import scena from '../dodatki/scena.jpg';
 
+import Spinner from 'react-bootstrap/Spinner';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
@@ -12,6 +13,14 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 export default function Sala(props) {
 
     const {id_wydarzenie,id_sala} = props.match.params
+
+    const [loading, setLoading] = useState(true)
+
+    const [wybraneMiejsce, setWybraneMiejsce] = useState(
+        {
+            numer_miejsca: 0
+        }
+    )
 
     const [sala, setSala] = useState(
         {
@@ -47,6 +56,7 @@ export default function Sala(props) {
         })
         .then(json => {
             setZajeteMiejsca(json)
+            setLoading(false)
         })
         .catch(err => console.error(err) )
     }
@@ -56,67 +66,91 @@ export default function Sala(props) {
         czytajRezerwacja()
     },[])
 
-    const wyswietlZajete = () => {
-
-        zajeteMiejsca.map((numer) => 
-            <li>{numer}</li>
-        )
-
-    }
-
-
+    
     const funkcjaKrzesla = () => {
 
-        console.log(zajeteMiejsca)
-
         let results = []
+
+        let getZajeteMiejsca = zajeteMiejsca.map(function(miejsce){
+            return miejsce['numer_miejsca']
+        })
+
+        let zajeteMiejscaIntArray = getZajeteMiejsca.splice(',').map(function(item){
+            return parseInt(item,10)
+        })
+
         let z = 1
         for(let i = 1; i<= sala.liczba_miejsc/10; i++){
             
-            results.push(<div><ButtonGroup></ButtonGroup></div>)
-
+            results.push(<div>Rząd {i}</div>)           
+            
             for(let k = 1; k<= 10; k++){
-                results.push((
-                     <Button variant="primary" disabled value = {z} key = {z} style={{width: '50px', height: '60px'}}>{z}</Button>                    
-                ))
-                z++
+
+                if (zajeteMiejscaIntArray.includes(z)){
+                    results.push((
+                    <Button variant="danger" name = "numer_miejsca" disabled value = {z} key = {z} style={{width: '50px', height: '60px'}} onClick = {handleClick}>{z}</Button>                                            
+                   ))
+                   z++
+                }else{
+                    results.push((
+                        <Button variant="primary" name = "numer_miejsca" value = {z} key = {z} style={{width: '50px', height: '60px'}} onClick = {handleClick}>{z}</Button>                    
+                    ))
+                    z++
+                }
             }
         }
 
-        results.push(<div><ButtonGroup></ButtonGroup></div>)
+        results.push(<div> Potwierdź wybór</div>)
         for(let h = 1; h<= sala.liczba_miejsc%10; h++){
 
 
-            results.push((
-                <Button variant="secondary" value = {z} key = {z} style={{width: '50px', height: '60px'}}> {z} </Button>
-           ))
-           z++
+            if (zajeteMiejscaIntArray.includes(z)){
+                results.push((
+                <Button variant="danger" name = "numer_miejsca" disabled value = {z} key = {z} style={{width: '50px', height: '60px'}} onClick = {handleClick}>{z}</Button>                                            
+               ))
+               z++
+            }else{
+                results.push((
+                    <Button variant="primary" name = "numer_miejsca" value = {z} key = {z} style={{width: '50px', height: '60px'}} onClick = {handleClick}>{z}</Button>                    
+                ))
+                z++
+            }
+
+
         }
         return results
 
     }
+    
+    const handleClick = (event) => {
+        event.preventDefault()
 
+    }
     const saladisplay =  
     (
-	    <div style={{width: '35rem', marginLeft: 'auto', marginRight: 'auto', paddingBottom: '40px'}}>
-	    	
-		
-		    		<img src={scena} />
-		
+	    <div style={{width: '35rem', marginLeft: 'auto', marginRight: 'auto', paddingBottom: '40px'}}>   			
+		    <img src={scena} />	
 	    </div>
     )
 
- 
+    const datadisplay = loading ? (
+        <Spinner animation="border" />
+    ) : (
+        <div>           
+        {funkcjaKrzesla()}
+        </div>
+    )
+
+
+
 
     return (
     <>
     	<div>
-    		{saladisplay}
-            <h2> Ilość miejsc - {sala.liczba_miejsc}</h2>
-            <h2> Ilość miejsc - {zajeteMiejsca[0].numer_miejsca}</h2>
-            {/* <h2> Ilość miejsc - {zajeteMiejsca[].numer_miejsca}</h2> */}
             <div style={{width: '35rem', marginLeft: 'auto', marginRight: 'auto'}} >
-                {funkcjaKrzesla()}
+                {saladisplay}
+                {datadisplay}
+                <Button onClick = {handleClick}> Rezerwuj</Button>
             </div>
        	</div>
 
